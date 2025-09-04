c4835f9331f356eb0996b2d1f7d2a12d517609e9
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-09-04 12:07:32 +0530

    making graph data object by pytorch tensor

--- bioPython/main.py
+++ bioPython/main.py
@@ -2,35 +2,37 @@ from Bio.PDB import *
 import numpy as np
 
 parser = PDBParser()
-structure = parser.get_structure("example", "/Users/rajan/github/proteinComplex/sqlProtein/1a3n.pdb")
-# for model in structure:
-#     for chain in model:
-#         for residue in chain:
-#             for atom in residue:
-#                 print(atom)
-#                 print(atom.get_name())
-#                 print(atom.get_coord())
+# structure = parser.get_structure("haemoglobin", "/Users/rajan/github/proteinComplex/sqlProtein/1a3n.pdb")
+structure = parser.get_structure("cellGrowthProtein", "/Users/rajan/github/proteinComplex/sqlProtein/P00533.pdb")
+for model in structure:
+    for chain in model:
+        for residue in chain:
+            print(residue)
+            # for atom in residue:
+            #     print(atom)
 
 
 
 
 # by geting the atoms coordinates we can calculate distances between atoms and by its coodinates we can also visualize the structure in 3D
 # do the above things for 1a3n.pdb file
-# atoms = []
-# for atom in structure.get_atoms():
-#     atoms.append(atom)
+"""
+atoms = []
+for atom in structure.get_atoms():
+    atoms.append(atom)
 
-# # Example: distance between first two atoms
-# coord1 = atoms[0].get_coord()
-# coord2 = atoms[1].get_coord()
+# Example: distance between first two atoms
+coord1 = atoms[0].get_coord()
+coord2 = atoms[1].get_coord()
 
-# distance = np.linalg.norm(coord1 - coord2)
-# print("Distance between atom 1 and atom 2:", distance, "Å")
-
+distance = np.linalg.norm(coord1 - coord2)
+print("Distance between atom 1 and atom 2:", distance, "Å")
+"""
 
 
 
 # Visualizing the structure in 3D using matplotlib
+"""
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -50,7 +52,7 @@ ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
 plt.show()
-
+"""
 
 
 
@@ -72,3 +74,37 @@ for i in range(n):
         dist_matrix[j, i] = dist
 print("Distance matrix between C-alpha atoms:")
 print(dist_matrix)
+
+
+
+
+
+import torch
+from torch_geometric.data import Data
+
+# Suppose we have N residues
+N = dist_matrix.shape[0]
+
+# Node features (example: random for now)
+x = torch.rand(N, 10)   # N nodes, 10 features each
+
+# Edge index from distance matrix
+threshold = 5.0
+edge_index = []
+edge_attr = []
+
+for i in range(N):
+    for j in range(N):
+        if i != j and dist_matrix[i, j] < threshold:
+            edge_index.append([i, j])
+            edge_attr.append([dist_matrix[i, j]])  # edge feature = distance
+
+edge_index = torch.tensor(edge_index).t().contiguous()
+edge_attr = torch.tensor(edge_attr, dtype=torch.float)
+
+# Graph object
+data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+print(data)
+print(data.x)
+print(data.edge_index)
+print(data.edge_attr)
