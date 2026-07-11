b887786b19615c3c5084103bad90d2a3d8188c3f
rajan-shubham <SHUBHAMRAJAN@proton.me>
2026-07-11 19:01:18 +0530

    update pkl graph data object count py file

--- complexPrediction8sem/pklGraphDataCount.py
+++ complexPrediction8sem/pklGraphDataCount.py
@@ -1,9 +1,16 @@
 import pickle
 
-with open("complexPrediction8sem/proteinGraphs.pkl", "rb") as f:
+with open("/Users/rajan/github/proteinComplex/DataCleaning/proteinGraphsIndexed.pkl", "rb") as f:
     data = pickle.load(f)
 
-names = [d.name for d in data]
+# names = [d.name for d in data]
+
+breaker = 0
+for d in data:
+    print(d)
+    breaker += 1
+    if(breaker == 24):
+        break
 
 print("Total graphs:", len(names))
 print("Unique graphs:", len(set(names)))
